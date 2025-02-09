f377291d062002d91345595c36c12ad95bce7a97
hieucoding123 <phamhieu211206@gmail.com>
2025-02-09 21:29:12 +0700

    Create window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+﻿#include <iostream>
+#include <SDL.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-int main()
+int main(int argc, char* argv[])
 {
-	cout << "Hello world" << endl;
+	SDL_Init(SDL_INIT_EVERYTHING); // Khởi tạo mọi thứ (video, hình ảnh, ... )
+
+	SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	// tạo cửa sổ ở giữa màn hình
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	// tạo trình dựng
+
+	SDL_SetRenderDrawColor(renderer, 255, 200, 200, 255); // tô màu rgb và alpha
+	SDL_RenderClear(renderer); // xóa trình dựng trước đó bằng màu ở trên
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(1000);	// Thời gian show cửa sổ
+
 	return 0;
 }
