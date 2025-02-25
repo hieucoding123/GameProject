b6e8b18fc0ff0d9431b2eb9c5c12ebba11371516
hieucoding123 <phamhieu211206@gmail.com>
2025-02-25 22:03:59 +0700

    add TransformComponent

--- include/ECS/TransformComponent.h
+++ include/ECS/TransformComponent.h
@@ -1,18 +1,32 @@
 #pragma once
 
 #include "Components.h"
+#include "Vector2D.h"
 
 class TransformComponent : public Component
 {
 private:
 	
 public:
-	TransformComponent() = default;
-	~TransformComponent();
+	Vector2D position;
+	int high;
+	int width;
+	int scale;
+	TransformComponent()
+	{
+		position.x = 0;
+		position.y = 0;
+	}
+	TransformComponent(float xpos, float ypos)
+	{
+		position.x = xpos;
+		position.y = ypos;
+	}
 
 	void init() override
 	{
-
+		position.x = 0;
+		position.y = 0;
 	}
 	void update() override
 	{
