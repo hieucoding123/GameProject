3502e9b451488c8723754c3b7f9129ee135e8a5d
hieucoding123 <phamhieu211206@gmail.com>
2025-02-25 23:41:44 +0700

    add TileComponent

--- include/ECS/Components.h
+++ include/ECS/Components.h
@@ -1,4 +1,8 @@
 #pragma once
 
+#include "Const.h"
+
 #include "ECS.h"
 #include "TransformComponent.h"
+#include "SpriteComponent.h"
+#include "TileComponent.h"
---
+++ include/ECS/TileComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Components.h"
+
+class TileComponent : public Component
+{
+public:
+	SDL_Texture* texture;
+	SDL_Rect srcRect, destRect;
+
+	TileComponent() = default;
+	TileComponent(int tileX, int tileY, int xpos, int ypos)
+	{
+		texture = TextureManager::LoadTexture(TILE_MAP);
+
+		srcRect.x = tileX;
+		srcRect.y = tileY;
+		srcRect.w = srcRect.h = BLOCK_H;
+
+		destRect.x = xpos;
+		destRect.y = ypos;
+		destRect.w = destRect.h = BLOCK_H * MAP_SCALE;
+	}
+
+	void draw() override
+	{
+		TextureManager::Draw(texture, &srcRect, &destRect);
+	}
+};
