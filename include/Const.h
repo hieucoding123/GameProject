5f8c9944686039244fdc246d6de18c05ab4bd924
hieucoding123 <phamhieu211206@gmail.com>
2025-02-25 23:42:16 +0700

    add paths, map scale

--- include/Const.h
+++ include/Const.h
@@ -6,10 +6,16 @@ const int HIGH = 600;
 const int FPS = 60;
 const int frameDelay = 1000 / FPS;
 
+const int BLOCK_W = 32;
+const int BLOCK_H = 32;
+const int MAP_SCALE = 1;
 
-const int blockW = 32;
-const int blockH = 32;
+const int FILE_COL = 16;
+const int FILE_ROW = 16;
 
-const int fileCol = 16;
-const int fileRow = 16;
+const int DEFAULT_X1 = 64;
+const int DEFAULT_X2 = 736;
+const int DEFAULT_Y = 568;
 
+const char* TILE_PATH = "assets/imageTileMap.png";
+const char* TILE_MAP = "assets/TileMap.txt";
