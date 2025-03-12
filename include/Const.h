ca363bd9f6ef5821ba8a3e11f6c2977c0586a8ae
hieucoding123 <phamhieu211206@gmail.com>
2025-03-12 17:12:58 +0700

    thay tên hành động bằng giá trị của phím

--- include/Const.h
+++ include/Const.h
@@ -25,7 +25,7 @@ const int DEFAULT_X1 = 64;
 const int DEFAULT_X2 = 736;
 const int DEFAULT_Y = 568;
 
-const int GROUND = 928;
+const int GROUND = 960;
 const int GRAVITY = 7;
 
 const int PLAYER1ID = 1;
@@ -49,25 +49,29 @@ const SDL_Color ENG_COLOR = { 73, 222, 255, 255 };
 constexpr const char* imageTilePath = "assets/imageTileMap.png";
 constexpr const char* tileMapPath = "assets/TileMap.txt";
 
-const std::unordered_map<int, const std::map<const char*, std::vector<int>>> GAMECHARACTERS = {
-		// frames - speed - srcX - srcY - W - H - isHitting - damage
-	{ 1, {{ "idle", {4, 150, 0,  0, 48, 75, 0, 0} },		// idle
-		{ "run", {6, 110, 0, 75, 67, 75, 0, 0}},			// walk
-		{ "jump", {3, 200, 0, 150, 56, 75, 0, 0}},		// jump
-		{ "fall", {1, 1, 0, 150, 56, 74, 0, 0}},
-		{ "guard", {1, 1, 0, 224, 58, 71, 0, 0}},
-		{"airguard", {1, 1, 0, 296, 58, 74, 0, 0}},
-		{"attackcb", {7, 180, 0, 371, 91, 75, 1, 20}},
-		{"skill1", {8, 100, 0, 446, 60, 77, 0, 0}},
-		{"eff1", {7, 140, 242, 0, 86, 57, 1, 20}},
-		{"takedam" ,{3, 250, 450, 77, 57, 73, 0, 0}}}},
-	{ 2, {{"idle", {6, 150, 0, 0, 46, 80, 0, 0}},
-		{"attackcb", {9, 110, 0, 80, 88, 86, 1, 20}},
-		{"run", {8, 150, 0, 166, 58, 77, 0, 0}},
-		{"jump", {3, 200, 0, 243, 68, 84, 0, 0}},
-		{"skill1", {8, 150, 0, 327, 84, 77, 0, 0}},
-		{"eff1", {12, 140, 303, 0, 68, 78, 1, 20}},
-		{"takedam", {1, 1000, 492, 169, 63, 74, 0, 0}}}}
+const std::map<int, std::vector<int>> SASUKE = {
+	// state - f - s - x - y- w - h - hit - damage
+	{ -1,				{4, 150, 0,  0, 48, 75, 0, 0} },		// idle
+	{ (int)SDLK_d,		{6, 110, 0, 75, 67, 75, 0, 0}},			// walk
+	{ (int)SDLK_w,		{3, 200, 0, 150, 56, 75, 0, 0}},		// jump
+	{ -2,				{1, 1, 0, 150, 56, 74, 0, 0} },
+	{ (int)SDLK_c,		{1, 1, 0, 224, 58, 71, 0, 0}},
+	{ (int)SDLK_k,		{1, 1, 0, 296, 58, 74, 0, 0}},
+	{ (int)SDLK_u,		{7, 180, 0, 371, 91, 75, 1, 20}},
+	{ (int)SDLK_r,		{8, 100, 0, 446, 60, 77, 0, 0}},
+	{ -3,				{7, 140, 242, 0, 86, 57, 1, 20} },
+	{ -4,				{3, 250, 450, 77, 57, 73, 0, 0} }
+};
+
+const std::map<int, std::vector<int>> AKAINU = {
+	// state - f - s - x - y- w - h - hit - damage
+		{-1,			{6, 150, 0, 0, 46, 80, 0, 0}},
+		{(int)SDLK_u,	{9, 110, 0, 80, 88, 86, 1, 20}},
+		{(int)SDLK_d,	{8, 150, 0, 166, 58, 77, 0, 0}},
+		{(int)SDLK_w,	{3, 200, 0, 243, 68, 84, 0, 0}},
+		{(int)SDLK_r,	{8, 150, 0, 327, 84, 77, 0, 0}},
+		{-3,			{12, 140, 303, 0, 68, 78, 1, 20}},
+		{-4,			{1, 1000, 492, 169, 63, 74, 0, 0}}
 };
 
 const std::vector<const char*> MASKS = {
