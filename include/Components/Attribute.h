eaf54354324603437f7d4a7a7e0cd19804940f91
hieucoding123 <phamhieu211206@gmail.com>
2025-03-12 17:12:27 +0700

    Kiểu thuộc tính cho đối tượng

---
+++ include/Components/Attribute.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Const.h"
+
+struct Attribute
+{
+	double hp = HP;			// máu
+	int energy = 0;			// năng lượng
+	double damage = 0;		// sát thương
+	int defense = DEFENSE;	// phòng thủ
+	int state = -1;
+	bool onGround = true;	// chạm đất
+	bool isHitting = false;	// đánh / không	
+	bool hasCamera = false;
+};
