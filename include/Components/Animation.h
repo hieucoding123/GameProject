c49594097ac9a5d282b7facb44b246fc8b8d37b7
hieucoding123 <phamhieu211206@gmail.com>
2025-03-14 10:58:53 +0700

    thêm comment

--- include/Components/Animation.h
+++ include/Components/Animation.h
@@ -11,10 +11,31 @@ class Animation
 public:
 	Animation(Attribute* attribute, Vector2D* vel,
 		SDL_Rect* rect, const std::map<int, std::vector<int>>& FIGURE);
-	void setState(int s);										// đặt trạng thái
-	bool isFinished() const { return animFinished; }			// kiểm tra kết thúc
-	void setFinished(bool b);									// cho kết thúc
-	std::vector<int> getEffectInfor(int state);					// lấy thông tin hiệu ứng
+	
+	/*
+	* Tùy chỉnh trạng thái
+	* @param int trạng thái(key) có trong map dữ liệu hành động
+	*/
+	void setState(int s);
+	
+	/*
+	* Kiểm tra hành động đã kết thúc chưa
+	* @return true nếu hành động đã kết thúc
+	*/
+	bool isFinished() const { return animFinished; }
+	
+	/*
+	* Tùy chỉnh kết thúc hành động
+	* @param bool true nếu đặt hành động kết thúc
+	*/
+	void setFinished(bool b);
+
+	/*
+	* Lấy thông tin hiệu ứng
+	* @param int key trong map dữ liệu hành động
+	* @return 1 vector chứa thông tin để khởi tạo hiệu ứng
+	*/
+	std::vector<int> getEffectInfor(int key);
 	void update();
 	~Animation();
 private:
--- include/TextureManager.h
+++ include/TextureManager.h
@@ -6,14 +6,29 @@
 class TextureManager
 {
 public:
-
+	/*
+	* Tải texture khi có đường dẫn ảnh
+	* @param const char* đường dẫn đến ảnh
+	* @return SDL_Texture* trỏ đến texture
+	*/
 	static SDL_Texture* LoadTexture(const char* path);
 
+	/*
+	* Vẽ lên màn hình qua renderer
+	* @param SDL_Texture* con trỏ đến texture
+	* @param SDL_Rect* con trỏ đến srcRect
+	* @param SDL_Rect* con trỏ đến destRect
+	* @param SDL_RendererFlip quy định lật ảnh
+	*/
 	static void Draw(SDL_Texture* texture, SDL_Rect* srcRect, SDL_Rect* destRect, SDL_RendererFlip flip);
 
 	static void DrawHP(int x, int y, int w, const SDL_Color& color, const SDL_Color& bgColor);
 
 	static void DrawEnergy(int x, int y, int w, const SDL_Color& color, const SDL_Color& bgColor);
 	
+	/*
+	*Vẽ ảnh khi có va chạm
+	* @param SDL_Rect* chứa vị trí cần vẽ
+	*/
 	static void DrawCollisionImage(const SDL_Rect* rect);
 };
